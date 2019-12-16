1a767f0c5acb52f4afd6b185e6e99961b151f031
ameba23 <ameba23@systemli.org>
2019-12-16 23:23:44 +0100

    test for hex

--- README.md
+++ README.md
@@ -1,9 +1,46 @@
 # Paper key export
 
-- mnemonics
+For backing up cryptographic keys on paper.  Takes a array of keys with names and creates a PDF.
+
+- mnemonics using BIP 39 (keys must be between 16 and 32 bytes)
 - QR codes
 - Removes files securely using [skrub](https://github.com/dawsbot/skrub)
+
+## Example:
+```js
+const paperKeys = require('.')
+const crypto = require('crypto')
+const keys = [
+  {
+    name: 'My important key',
+    data: crypto.randomBytes(32)
+  },
+  {
+    name: 'Another important key',
+    data: crypto.randomBytes(32)
+  }
+]
+
+paperKeys.createPdf('./key-backup.pdf', keys, (err) => {
+  if (err) throw err
+})
+```
 
 ## Example PDF output:
 
 ![example](./example.png)
+
+## API
+
+### `createPdf (outputFile, keys, callback)`
+
+creates a pdf `outputFile`.
+- `keys` should be an array of objects of the form:
+  - `name` - a string describing the key.
+  - `data` - the key as a buffer or hex encoded string.
+
+### `removePdf (filePath)`
+Securely removes the given file.  Returns a promise.
+
+### `mnemonicToKey (mnemonic)`
+Turn a mnemonic back into a key. Takes a string, returns a buffer.
--- index.js
+++ index.js
@@ -2,8 +2,9 @@ const markdownpdf = require('markdown-pdf')
 const QRCode = require('qrcode')
 const bip39 = require('bip39')
 const skrub = require('skrub')
-const path = require('path')
 const tmp = require('tmp')
+const path = require('path')
+const assert = require('assert')
 
 tmp.setGracefulCleanup()
 
@@ -13,9 +14,11 @@ async function createPdf (outputFile, keys, callback) {
 
   const filenames = await Promise.all(keys.map(async (key, index) => {
     const filename = path.join(tmpObj.name, `${index}.png`)
+    const data = isHexString(key.data) ? Buffer.from(key.data, 'hex') : key.data
+    assert(Buffer.isBuffer(data))
     await QRCode.toFile(
       filename,
-      [{ data: key.data, mode: 'byte' }],
+      [{ data, mode: 'byte' }],
       {}
     )
     markdown += `## ${key.name}\n![qrcode](${filename})\n`
@@ -42,7 +45,13 @@ function removePdf (filePath) {
 }
 
 function mnemonicToKey (mnemonic) {
-  return bip39.mnemonicToEntropy(mnemonic) // Buffer.from 'hex' ?
+  return Buffer.from(bip39.mnemonicToEntropy(mnemonic), 'hex')
+}
+
+function isHexString (thing, length) {
+  if (typeof thing !== 'string') return false
+  if (length && (thing.length !== length)) return false
+  return RegExp('[0-9a-fA-F]+').test(thing)
 }
 
 module.exports = { createPdf, removePdf, mnemonicToKey }
